2f44683bcc86a1de3688368d01a183fbe2afe64f
Marko Justinek <marko.justinek@ittybittyapps.com>
2019-06-24 12:01:32 +1000

    Update to v1.68.0

--- pact-ruby-standalone.rb
+++ pact-ruby-standalone.rb
@@ -1,22 +1,22 @@
 class PactRubyStandalone < Formula
-  desc "A standalone pact command line executable using the ruby pact implementation and Travelling Ruby"
-  homepage "https://github.com/pact-foundation/pact-ruby-standalone/"
-  url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.67.0/pact-1.67.0-osx.tar.gz"
-  version "1.67.0"
-  sha256 "6a7e789477ec14a5dda61010e4460b1007c02719bdcfe25e789549b6e5497add"
+    desc "A standalone pact command line executable using the ruby pact implementation and Travelling Ruby"
+    homepage "https://github.com/pact-foundation/pact-ruby-standalone"
+    url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/1.68.0/pact-1.68.0-osx.tar.gz"
+    version "1.68.0"
+    sha256 "baab387f5c576a33fe5178bb43789b1b694950f5a56133af24a9a558475b080d"
 
-  def install
-    bin.install Dir["bin/*"]
-    lib.install Dir["lib/*"]
+    def install
+        bin.install Dir["bin/*"]
+        lib.install Dir["lib/*"]
 
-    puts "# Usage `pact-mock-service help [COMMAND]`"
-    puts "#"
-    puts "# For other tools related to PACT in this bundle see https://github.com/pact-foundation/pact-ruby-standalone/releases/"
-    puts "#"
-  end
+        puts "# Usage: pact-mock-service help [COMMAND]"
+        puts "#"
+        puts "# For other tools related to PACT in this bundle see https://github.com/pact-foundation/pact-ruby-standalone/releases/"
+        puts "#"
+    end
 
-  test do
-    system "#{bin}/pact-mock-service", "help"
-  end
+    test do
+        system "#{bin}/pact-mock-service", "help"
+    end
 
 end
