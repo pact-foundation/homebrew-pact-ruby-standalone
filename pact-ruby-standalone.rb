e820bfd2510fd9167c088114db33d6ba190ba913
Marko Justinek <marko.justinek@ittybittyapps.com>
2019-12-09 11:03:03 +1100

    Release of version v1.72.2 (#6)

--- pact-ruby-standalone.rb
+++ pact-ruby-standalone.rb
@@ -1,9 +1,9 @@
 class PactRubyStandalone < Formula
     desc "A standalone pact command line executable using the ruby pact implementation and Travelling Ruby"
     homepage "https://github.com/pact-foundation/pact-ruby-standalone"
-    url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.69.0/pact-1.69.0-osx.tar.gz"
-    version "1.69.0"
-    sha256 "87b3aa5d3a2789b6c39f9bca58948033dfc4dc48438dd59bf9280c10f48b1da6"
+    url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.72.2/pact-1.72.2-osx.tar.gz"
+    version "1.72.2"
+    sha256 "7a18613aec5729321bb8a401ff97780e7bc75467e7b4bfe50a48dac1583f13e0"
 
     def install
         bin.install Dir["bin/*"]
