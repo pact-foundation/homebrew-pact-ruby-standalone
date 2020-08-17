607e3c15a1248827a5c95aa8f7fcd29b29a9ab7a
Lars Gohr <elgohr@oss.volkswagen.com>
2020-08-17 12:57:52 +0200

    ci: Release of version v1.88.3

--- pact-ruby-standalone.rb
+++ pact-ruby-standalone.rb
@@ -1,8 +1,8 @@
 class PactRubyStandalone < Formula
     desc "A standalone pact command line executable using the ruby pact implementation and Travelling Ruby"
     homepage "https://github.com/pact-foundation/pact-ruby-standalone"
-    url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.84.0/pact-1.84.0-osx.tar.gz"
-    version "1.84.0"
+    url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.88.3/pact-1.88.3-osx.tar.gz"
+    version "1.88.3"
     sha256 "73fa53ec53e2e53243cf8cb21feabb8245a7b42e2c0c82e3dfa57906ea983fc0"
 
     def install
