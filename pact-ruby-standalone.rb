6727323da5dd6ebf20bee2283c1b33f7667e66e6
bethesque <bethesque@users.noreply.github.com>
2021-09-21 21:50:32 +0000

    chore(release): Update version to v1.88.67

--- pact-ruby-standalone.rb
+++ pact-ruby-standalone.rb
@@ -1,9 +1,9 @@
 class PactRubyStandalone < Formula
     desc "A standalone pact command line executable using the ruby pact implementation and Travelling Ruby"
     homepage "https://github.com/pact-foundation/pact-ruby-standalone"
-    url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.88.66/pact-1.88.66-osx.tar.gz"
-    version "1.88.66"
-    sha256 "2d509b4591c2a72bc34d787ff8263f1e5ae539c5a05b842279b4d59d81b479b7"
+    url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v1.88.67/pact-1.88.67-osx.tar.gz"
+    version "1.88.67"
+    sha256 "708a07ba9b4629d68e02a1fdc9114e4ee0e0245762b57b188ad4e382dfc4cb36"
 
     def install
         bin.install Dir["bin/*"]
