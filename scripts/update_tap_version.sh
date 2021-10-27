142866ae269928060e7449875b59c41b01e9e05b
Marko Justinek <1733327+surpher@users.noreply.github.com>
2021-10-28 10:48:48 +1100

    Update update_tap_version.sh

--- scripts/update_tap_version.sh
+++ scripts/update_tap_version.sh
@@ -24,7 +24,7 @@ write_homebrew_formulae() {
         echo "    bin.install Dir[\"bin/*\"]" >&3
         echo "    lib.install Dir[\"lib/*\"]" >&3
         echo "" >&3
-        echo "    puts \"# Run 'pact-mock-service help' (see $homepage/releases/)\"" >&3
+        echo "    puts \"# Run 'pact-mock-service --help' (see $homepage/releases/)\"" >&3
         echo "  end" >&3
         echo "" >&3
         echo "  test do" >&3
