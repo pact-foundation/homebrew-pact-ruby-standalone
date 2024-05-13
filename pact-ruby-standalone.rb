693c1b6302843f542aebff39340d2215ee622e03
pact-dev <pact-dev@users.noreply.github.com>
2024-05-13 18:00:24 +0000

    chore(release): Update version to v2.4.3

--- pact-ruby-standalone.rb
+++ pact-ruby-standalone.rb
@@ -1,25 +1,25 @@
 class PactRubyStandalone < Formula
   desc "Standalone pact CLI executable using the Ruby Pact impl and Travelling Ruby"
   homepage "https://github.com/pact-foundation/pact-ruby-standalone"
-  version "2.4.2"
+  version "2.4.3"
   on_macos do
     on_intel do
-      url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v2.4.2/pact-2.4.2-osx-x86_64.tar.gz"
-      sha256 "69ca61166edb0a8a3329c28a100409be949aefd0a64d413450f6cd59b0db357d"
+      url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v2.4.3/pact-2.4.3-osx-x86_64.tar.gz"
+      sha256 "02f3bfcbf49e12f8464fc1ccb1c7c027d4a3db61b59d6ff05c587de4aeadcd6a"
     end
     on_arm do
-      url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v2.4.2/pact-2.4.2-osx-arm64.tar.gz"
-      sha256 "cd52b00efc7b9c8fc5996fa8d292a16d51ff69b7172044f584dc4dc0f55ba0a1"
+      url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v2.4.3/pact-2.4.3-osx-arm64.tar.gz"
+      sha256 "3d33330dff06eb4811e1bcdd7abcbe5fc1f1ff1e1a79094f4ad0aaf866046485"
     end
   end
   on_linux do
     on_intel do
-      url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v2.4.2/pact-2.4.2-linux-x86_64.tar.gz"
-      sha256 "fb405fc853cb68740bcff6803345a06630c22fd9f41b1ca1e7d4bf4d8eed3be1"
+      url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v2.4.3/pact-2.4.3-linux-x86_64.tar.gz"
+      sha256 "9c8c8a495f848e86accc5d547ccf03186f2bf70a2bcd97ac0be1bf79bdcba99e"
     end
     on_arm do
-      url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v2.4.2/pact-2.4.2-linux-arm64.tar.gz"
-      sha256 "fa4bb5d8856ea5fec5f6f33a6d499f08f4834b6a1ce2e441f265b90de93a5f7e"
+      url "https://github.com/pact-foundation/pact-ruby-standalone/releases/download/v2.4.3/pact-2.4.3-linux-arm64.tar.gz"
+      sha256 "39f422022808b93f1b2a0f0f68b22c7b1eb32cb8d6ced56ec325a9e8f1b2dcae"
     end
   end
 
